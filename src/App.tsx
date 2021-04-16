52b5938d834fbf37133537042655d625607f6218
eshell <eshellster@gmail.com>
2021-04-16 21:09:38 +0900

    feat: Apollo Setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="bg-black">
-      <h1 className=" text-xl text-white ">Hello Tailwind</h1>
+      <h1 className="text-xl text-white">Hello Tailwind</h1>
     </div>
   );
 }
