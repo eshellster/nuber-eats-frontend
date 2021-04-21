037f6fa257de3c2086575d309f3cac11c813c64a
eshell <eshellster@gmail.com>
2021-04-21 14:57:00 +0900

    feat: UI clone

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lime: colors.lime,
+      },
+    },
   },
   variants: {
     extend: {},
