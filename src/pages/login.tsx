eb828898b527c1758a8776a43533e4ed0e1785d5
eshell <eshellster@gmail.com>
2021-04-21 14:56:33 +0900

    feat: UI clone

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { LoginValidationSchema } from "../yup/loginVaidationSchema";
 import { gql, useMutation } from "@apollo/client";
+import noberLogo from "../images/logo.svg";
 import {
   loginMutation,
   loginMutationVariables,
 } from "../__generated__/loginMutation";
+import { Button } from "../components/button";
+import { Link } from "react-router-dom";
 
 const LOGIN_MUTATION = gql`
   mutation loginMutation($loginInput: LoginInput!) {
@@ -29,10 +33,10 @@ export const Login = () => {
     register,
     getValues,
     handleSubmit,
-    watch,
-    formState: { errors, isDirty },
+    formState: { errors, isValid },
   } = useForm<ILoginFormInput>({
     resolver: yupResolver(LoginValidationSchema),
+    mode: "onChange",
   });
 
   const onCompleted = (data: loginMutation) => {
@@ -67,12 +71,15 @@ export const Login = () => {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-gray-50 w-full max-w-lg py-5 rounded-lg text-center">
-        <h3 className="text-2xl text-gray-800">Log In</h3>
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-32">
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={noberLogo} className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">
+          Welcome back
+        </h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 px-5 mt-5"
+          className="grid gap-3 mt-5 w-full"
         >
           <input
             {...register("email")}
@@ -96,13 +103,17 @@ export const Login = () => {
               {errors.password?.message}
             </span>
           )}
-          <button className="btn mt-3">
-            {loading ? "loading..." : "Log In"}
-          </button>
-          <span className="font-medium text-red-500">
+          <Button canClick={isValid} loading={loading} actionText={"Log in"} />
+          <span className="font-medium text-red-500 mb-4">
             {loginMutationResult?.login.error}
           </span>
         </form>
+        <div>
+          New to Nuber?{" "}
+          <Link to="/create-account" className="text-lime-700 hover:underline">
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   );
