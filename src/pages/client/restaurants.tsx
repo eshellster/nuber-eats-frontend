cbaeeaafa2d7820a8c04814bacae2087908ca6bf
eshell <eshellster@gmail.com>
2021-04-25 22:23:27 +0900

    style: 카테고리

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,3 +1,83 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import {
+  restaurantsPageQuery,
+  restaurantsPageQueryVariables,
+} from "../../__generated__/restaurantsPageQuery";
 
-export const Restaurants = () => <h1>Restaurants</h1>;
+const RESTAURANTS_QUERY = gql`
+  query restaurantsPageQuery($restaurantsInput: RestaurantsInput!) {
+    allCategories {
+      ok
+      error
+      categories {
+        id
+        name
+        coverImg
+        slug
+        restaurantCount
+      }
+    }
+    restaurants(input: $restaurantsInput) {
+      ok
+      error
+      totalPages
+      totalResults
+      results {
+        id
+        name
+        coverImg
+        category {
+          name
+        }
+        address
+        isPromoted
+      }
+    }
+  }
+`;
+
+export const Restaurants = () => {
+  const { data, loading, error } = useQuery<
+    restaurantsPageQuery,
+    restaurantsPageQueryVariables
+  >(RESTAURANTS_QUERY, {
+    variables: {
+      restaurantsInput: {
+        page: 1,
+        limit: 20,
+      },
+    },
+  });
+  return (
+    <div>
+      <form className="bg-gray-800 w-full py-40 flex items-center justify-center">
+        <input
+          type="Search"
+          className="input rounded-md border-0 w-3/12"
+          placeholder="Search restaurants..."
+        />
+      </form>
+      {!loading && (
+        <div className="max-w-screen-xl mx-auto mt-8">
+          <div className="overflow-x-auto flex max-w-5xl mx-auto ">
+            {data?.allCategories.categories?.map((category, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center cursor-pointer px-5"
+              >
+                <div
+                  className="w-24 h-24 bg-cover hover:bg-gray-100 rounded-full"
+                  style={{ backgroundImage: `url(${category.coverImg})` }}
+                ></div>
+                <span className="mt-1 text-sm text-center font-medium">
+                  {category.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
