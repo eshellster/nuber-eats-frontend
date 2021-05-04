f011438b41a6de7d049425f6ae924f00e96d9ba4
eshell <eshellster@gmail.com>
2021-05-04 17:40:11 +0900

    feat: Router 사용자 롤별로 정리

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,40 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useMe } from "../hooks/useMe";
 import { Restaurants } from "../pages/client/restaurants";
 import { Header } from "../components/header";
 import { ConfirmEmail } from "../pages/user/confirmEmail";
-import { UserRole } from "../__generated__/globalTypes";
 import { EditProfile } from "../pages/user/edit-profile";
 import { SearchRestaurants } from "../pages/client/searchRestaurants";
 import { CategoryRestaurants } from "../pages/client/categoryRestaurants";
 import { Restaurant } from "../pages/client/restaurant";
+import { MyRestaurants } from "../pages/owner/my-restaurants";
+import { NotFound } from "../pages/404";
 
-const ClientRoutes = [
-  <Route key={1} path="/" exact>
-    <Restaurants />
-  </Route>,
-  <Route key={2} path="/confirm">
-    <ConfirmEmail />
-  </Route>,
-  <Route key={3} path="/edit-profile">
-    <EditProfile />
-  </Route>,
-  <Route key={4} path="/search-restaurants">
-    <SearchRestaurants />
-  </Route>,
-  <Route key={5} path="/category-restaurants/:slug">
-    <CategoryRestaurants />
-  </Route>,
-  <Route key={6} path="/restaurant/:id">
-    <Restaurant />
-  </Route>,
+const clientRoutes = [
+  { path: "/", component: <Restaurants /> },
+  { path: "/search-restaurants", component: <SearchRestaurants /> },
+  { path: "/category-restaurants/:slug", component: <CategoryRestaurants /> },
+  { path: "/restaurant/:id", component: <Restaurant /> },
 ];
+
+const commonRoutes = [
+  { path: "/confirm", component: <ConfirmEmail /> },
+  { path: "/edit-profile", component: <EditProfile /> },
+];
+
+const restaurantRoutes = [{ path: "/", component: <MyRestaurants /> }];
 
 export const LoggedInRouter = () => {
   const { data, loading, error } = useMe();
@@ -49,8 +38,26 @@ export const LoggedInRouter = () => {
     <Router>
       <Header email={data.me.email} />
       <Switch>
-        {data.me.role === UserRole.Client && ClientRoutes}
-        <Redirect to="/" />
+        {data.me.role === "Client" &&
+          clientRoutes.map((route) => (
+            <Route key={route.path} path={route.path}>
+              {route.component}
+            </Route>
+          ))}
+        {data.me.role === "Owner" &&
+          restaurantRoutes.map((route) => (
+            <Route key={route.path} path={route.path}>
+              {route.component}
+            </Route>
+          ))}
+        {commonRoutes.map((route) => (
+          <Route key={route.path} path={route.path}>
+            {route.component}
+          </Route>
+        ))}
+        <Route>
+          <NotFound />
+        </Route>
       </Switch>
     </Router>
   );
