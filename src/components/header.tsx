f35cff7c94ce5bb80678f2da832d251f15096207
eshell <eshellster@gmail.com>
2021-06-24 23:06:27 +0900

    feat: Restaurant Order

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,7 @@ import { authTokenVar, isLoggedInVar } from "../apollo";
 import { LOCALSTORAGE_TOKEN } from "../constants";
 import { useMe } from "../hooks/useMe";
 import nuberLogo from "../images/logo.svg";
+import { UserRole } from "../__generated__/globalTypes";
 
 interface IHeaderProps {
   email: string;
@@ -15,6 +16,8 @@ interface IHeaderProps {
 export const Header: React.FC<IHeaderProps> = ({ email }) => {
   const { data } = useMe();
   const client = useApolloClient();
+  console.log(data);
+
   return (
     <>
       {!data?.me.verified && (
@@ -26,6 +29,21 @@ export const Header: React.FC<IHeaderProps> = ({ email }) => {
         <div className="px-5 xl:px-0 w-full max-w-screen-xl mx-auto flex justify-between items-center">
           <Link to="/">
             <img src={nuberLogo} className="w-24" alt="Nuber Eats" />
+            {data?.me.role === UserRole.Client && (
+              <div className="mt-1 text-center font-bold text-white bg-yellow-600">
+                주문
+              </div>
+            )}
+            {data?.me.role === UserRole.Owner && (
+              <div className="mt-1 text-center font-bold text-white bg-red-700">
+                Restaurant
+              </div>
+            )}
+            {data?.me.role === UserRole.Delivery && (
+              <div className="mt-1 text-center font-bold text-white bg-indigo-600">
+                Driver
+              </div>
+            )}
           </Link>
 
           <span className="text-xs">
--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,13 +1,11 @@
-import { gql, useQuery, useSubscription } from "@apollo/client";
+import { gql, useQuery } from "@apollo/client";
 import React, { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router";
 import { FULL_ORDERS_FRAGMENT } from "../fragments";
+import { useMe } from "../hooks/useMe";
 import { getOrder, getOrderVariables } from "../__generated__/getOrder";
-import {
-  orderUpdates,
-  orderUpdatesVariables,
-} from "../__generated__/orderUpdates";
+import { orderUpdates } from "../__generated__/orderUpdates";
 
 const GET_ORDER = gql`
   query getOrder($input: GetOrderInput!) {
@@ -37,6 +35,7 @@ interface IParams {
 
 export const Order = () => {
   const params = useParams<IParams>();
+  const { data: userData } = useMe();
   const { data, subscribeToMore } = useQuery<getOrder, getOrderVariables>(
     GET_ORDER,
     {
@@ -47,7 +46,7 @@ export const Order = () => {
       },
     }
   );
-  const order = data?.getOrder.order;
+  // const order = data?.getOrder.order;
 
   // const { data: subscriptionData } = useSubscription<
   //   orderUpdates,
@@ -83,7 +82,7 @@ export const Order = () => {
         },
       });
     }
-  }, [data]);
+  }, [data, params, subscribeToMore]);
 
   return (
     <div className="mt-32 container flex justify-center">
@@ -94,26 +93,45 @@ export const Order = () => {
         <h4 className="bg-gray-800 w-full py-5 text-white text-center text-xl">
           Order #{params.id}
         </h4>
-        <h5 className="p-5 pt-10 text-3xl text-center ">{order?.total}₩</h5>
+        <h5 className="p-5 pt-10 text-3xl text-center ">
+          {data?.getOrder.order?.total}₩
+        </h5>
         <div className="p-5 text-xl grid gap-6">
           <div className="border-t pt-5 border-gray-700">
             Prepared By:{" "}
-            <span className="font-medium">{order?.restaurant?.name}</span>
+            <span className="font-medium">
+              {data?.getOrder.order?.restaurant?.name}
+            </span>
           </div>
           <div className="border-t pt-5 border-gray-700 ">
             Deliver To:{" "}
-            <span className="font-medium">{order?.customer?.email}</span>
+            <span className="font-medium">
+              {data?.getOrder.order?.customer?.email}
+            </span>
           </div>
           <div className="border-t border-b py-5 border-gray-700">
             Driver:{" "}
             <span className="font-medium">
-              {order?.driver?.email ? order?.driver?.email : "Not yet."}
+              {data?.getOrder.order?.driver?.email
+                ? data?.getOrder.order?.driver?.email
+                : "Not yet."}
             </span>
           </div>
-          <span className=" text-center mt-5 mb-3  text-xl ">
-            Status:
-            <span className="text-3xl text-lime-600"> {order?.status}</span>
-          </span>
+          {userData?.me.role === "Client" && (
+            <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">
+              Status: {data?.getOrder.order?.status}
+            </span>
+          )}
+          {userData?.me.role === "Owner" && (
+            <>
+              {data?.getOrder.order?.status === "Pending" && (
+                <button className="btn">Accept Order</button>
+              )}
+              {data?.getOrder.order?.status === "Cooking" && (
+                <button className="btn">Order Cooked</button>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
